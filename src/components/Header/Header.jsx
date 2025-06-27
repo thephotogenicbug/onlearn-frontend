a7d60179afe99e0f36831cf6059517eaef529b37
thephotogenicbug <navinkumarachari@gmail.com>
2025-06-27 16:11:27 +0530

    add mobile responsivness to header and navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,19 +12,19 @@ const Header = () => {
       <LayoutWrapper>
         <Navbar />
 
-        <div className="flex flex-row  justify-between relative">
+        <div className="flex items-center justify-center md:flex-row  md:items-start md:justify-between relative">
           {/* LEFT: Text Section */}
-          <div className="mt-24">
-            <button className="bg-white px-6 py-2 rounded-lg text-sm font-medium">
+          <div className="mt-24 flex flex-col justify-center items-center md:justify-start md:items-start  ">
+            <button className=" bg-white px-6 py-2 rounded-lg text-sm font-medium">
               Never Stop Learning
             </button>
-            <h1 className="text-[#0B7077] text-[50px] font-bold leading-tight mt-3">
+            <h1 className="text-[#0B7077] text-[30px] text-center md:text-start md:text-[50px] font-bold leading-tight mt-3">
               Grow up your skills by <br />
               online courses with <br />
               <span className="text-[#01767B]">Onlearning</span>
             </h1>
 
-            <div className="flex items-center gap-4 mt-6">
+            <div className="flex flex-col md:flex-row items-center gap-4 mt-6">
               <button className="bg-[#FD661F] py-3 px-6 rounded-lg text-white font-semibold cursor-pointer">
                 Explore Path
               </button>
@@ -47,11 +47,11 @@ const Header = () => {
               </div>
               <div>
                 <div>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-regular fa-star-half-stroke"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-solid fa-star"></i>
+                  <i className="fa-regular fa-star-half-stroke"></i>
                 </div>
                 <div>
                   <p className="text-sm font-semibold">(10k Reviews)</p>
@@ -61,10 +61,10 @@ const Header = () => {
           </div>
 
           {/* RIGHT: Man Image and Floating Boxes */}
-          <div className="relative">
+          <div className=" hidden md:flex relative">
             {/* Floating Calendar Card */}
             <div className="absolute left-20 bottom-[120px] z-20">
-              <div className="flex items-center bg-white/80 rounded-xl shadow-lg px-6 py-4">
+              <div className="hidden md:flex items-center bg-white/80 rounded-xl shadow-lg px-6 py-4">
                 <div className="bg-[#23BDEE] p-3 rounded-lg">
                   <img
                     src={assets.calendar}
@@ -85,7 +85,7 @@ const Header = () => {
             <img
               src={assets.header_img_man}
               alt="Student"
-              className="h-[550px] object-contain relative z-10"
+              className=" hidden md:flex md:h-[550px] object-contain relative z-10"
             />
 
             {/* Floating Graph Icon */}
@@ -93,12 +93,12 @@ const Header = () => {
               <img
                 src={assets.graph}
                 alt="Graph"
-                className="w-[140px] h-[140px] object-contain"
+                className="hidden md:flex md:w-[140px] md:h-[140px] object-contain"
               />
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center bg-[#D2E6E4] relative">
+        <div className="flex items-center justify-center bg-[#D2E6E4] relative mt-28 md:mt-0">
           <div className="flex items-center justify-center bg-[#D2E6E4] p-3 w-[180px] h-[180px] absolute rounded-full">
             <div className="bg-[#0B7077] flex flex-col justify-center items-center p-3 w-[150px] h-[150px] m-5 absolute rounded-full z-10">
               <svg
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const handleNavbar = (navbar) => {
+    setNavbar(navbar);
+  };
+
   return (
-    <div className=" w-full flex flex-row justify-between p-5 items-center">
-      <div>
-        <img src={assets.logo} alt="" className=" h-[2.5rem]" />
+    <>
+      <div className=" w-full flex flex-row justify-between p-5 items-center">
+        <div>
+          <img src={assets.logo} alt="" className=" h-[2.5rem]" />
+        </div>
+        <ul className=" hidden md:flex flex-row space-x-10 font-medium">
+          <li>
+            <a className="cursor-pointer hover:text-[#FD661F]">Home</a>
+          </li>
+          <li>
+            <a className="cursor-pointer hover:text-[#FD661F]">Careers</a>
+          </li>
+          <li>
+            <a className="cursor-pointer hover:text-[#FD661F]">Blogs</a>
+          </li>
+          <li>
+            <a className="cursor-pointer hover:text-[#FD661F]">About Us</a>
+          </li>
+        </ul>
+        <div className=" hidden md:flex flex-row space-x-10 px-10">
+          <button className="bg-white text-[#0B7077] px-8 py-3 rounded-lg uppercase cursor-pointer hover:bg-white/80">
+            Log in
+          </button>
+          <button className=" bg-[#0B7077] px-8 py-3 rounded-lg text-white uppercase cursor-pointer hover:bg-[#0B7077]/90">
+            Sign up
+          </button>
+        </div>
+        <div className=" flex md:hidden  items-center">
+          {navbar ? (
+            <i
+              onClick={() => handleNavbar(!navbar)}
+              className="fa-solid fa-xmark text-3xl cursor-pointer"
+            ></i>
+          ) : (
+            <i
+              onClick={() => handleNavbar(!navbar)}
+              className="fa-solid fa-bars text-3xl cursor-pointer"
+            ></i>
+          )}{" "}
+        </div>
       </div>
-      <ul className=" flex flex-row space-x-10 font-semibold">
-        <li>
-          <a className="cursor-pointer hover:text-[#FD661F]">Home</a>
-        </li>
-        <li>
-          <a className="cursor-pointer hover:text-[#FD661F]">Careers</a>
-        </li>
-        <li>
-          <a className="cursor-pointer hover:text-[#FD661F]">Blogs</a>
-        </li>
-        <li>
-          <a className="cursor-pointer hover:text-[#FD661F]">About Us</a>
-        </li>
-      </ul>
-      <div className=" flex flex-row space-x-10 px-10">
-        <button className="bg-white text-[#0B7077] px-8 py-3 rounded-lg uppercase cursor-pointer hover:bg-white/80">
-          Log in
-        </button>
-        <button className=" bg-[#0B7077] px-8 py-3 rounded-lg text-white uppercase cursor-pointer hover:bg-[#0B7077]/90">
-          Sign up
-        </button>
-      </div>
-    </div>
+      {navbar ? (
+        <div className="flex md:hidden w-full bg-white p-4 rounded-lg  ">
+          <ul className=" flex flex-col space-x-10 font-medium space-y-4">
+            <li className="flex items-center gap-2 pl-4 text-gray-600 hover:text-[#FD661F]">
+              <i className="fa-solid fa-circle-chevron-right "></i>
+              <a className="cursor-pointer hover:text-[#FD661F]">Home</a>
+            </li>
+            <li className="flex items-center gap-2 pl-4 text-gray-600 hover:text-[#FD661F]">
+              <i className="fa-solid fa-circle-chevron-right "></i>
+              <a className="cursor-pointer">Careers</a>
+            </li>
+            <li className="flex items-center gap-2 pl-4 text-gray-600 hover:text-[#FD661F]">
+              <i className="fa-solid fa-circle-chevron-right "></i>
+              <a className="cursor-pointer">Blogs</a>
+            </li>
+            <li className="flex items-center gap-2 pl-4 text-gray-600 hover:text-[#FD661F]">
+              <i className="fa-solid fa-circle-chevron-right "></i>
+              <a className="cursor-pointer">About Us</a>
+            </li>
+            <div className="flex md:hidden flex-row space-x-10 px-10">
+              <button className="bg-[#0B7077] text-white px-8 py-3 rounded-lg uppercase cursor-pointer hover:bg-[#0B7077]/90">
+                Log in
+              </button>
+              <button className=" bg-[#0B7077] px-8 py-3 rounded-lg text-white uppercase cursor-pointer hover:bg-[#0B7077]/90">
+                Sign up
+              </button>
+            </div>
+          </ul>
+        </div>
+      ) : null}
+    </>
   );
 };
 
